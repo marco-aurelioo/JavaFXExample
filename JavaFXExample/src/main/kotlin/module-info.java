ec40cd37cea7d729df39f2c6f71442d1661ad782
marco-aurelioo <marco_aurelioo@yahoo.com.br>
2022-08-24 23:57:20 -0300

    initial commit

---
+++ JavaFXExample/src/main/kotlin/module-info.java
@@ -0,0 +1,17 @@
+module com.crud.javafxexample {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires kotlin.stdlib;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.crud.javafxexample to javafx.fxml;
+    exports com.crud.javafxexample;
+}
